4a28b3e5dfe80e9a9f8f653fdb4ac88065e24e27
sasgas <sasgas@castis.com>
2020-06-18 11:03:26 +0900

    traceID 에 % 가 포함되는 경우에 대한 예외처리

--- clog/clog.go
+++ clog/clog.go
@@ -30,56 +30,56 @@ func SetDir(dir string) {
 
 // Debugf1 :
 func Debugf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.DEBUG, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.DEBUG, msg, time.Now())
 }
 
 // Reportf1 :
 func Reportf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.REPORT, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.REPORT, msg, time.Now())
 }
 
 // Infof1 :
 func Infof1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.INFO, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.INFO, msg, time.Now())
 }
 
 // Successf1 :
 func Successf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.SUCCESS, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.SUCCESS, msg, time.Now())
 }
 
 // Warningf1 :
 func Warningf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.WARNING, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.WARNING, msg, time.Now())
 }
 
 // Errorf1 :
 func Errorf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.ERROR, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.ERROR, msg, time.Now())
 }
 
 // Failf1 :
 func Failf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.FAIL, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.FAIL, msg, time.Now())
 }
 
 // Exceptionf1 :
 func Exceptionf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.EXCEPTION, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.EXCEPTION, msg, time.Now())
 }
 
 // Criticalf1 :
 func Criticalf1(traceID string, format string, v ...interface{}) {
-	format = "[" + traceID + "] " + format
-	cilog.StdLogger().Log(2, cilog.CRITICAL, fmt.Sprintf(format, v...), time.Now())
+	msg := "[" + traceID + "] " + fmt.Sprintf(format, v...)
+	cilog.StdLogger().Log(2, cilog.CRITICAL, msg, time.Now())
 }
 
 // Debugf :
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/castisdev/gcommon
 go 1.14
 
 require (
-	github.com/castisdev/cilog v0.0.0-20170713053253-1da4adb86e35
+	github.com/castisdev/cilog v0.0.0-20200602054520-34412660cb5b
 	github.com/juju/ratelimit v1.0.1
-	github.com/stretchr/testify v1.6.0
-	golang.org/x/sys v0.0.0-20200610111108-226ff32320da
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1
 	gopkg.in/yaml.v2 v2.3.0
 )
